f2825a24004df91a756e31a151061c5973c9463a
Leo <leo@opensignal.com>
2015-07-25 13:54:38 +0100

    Introduce PlotData class to compute, store and manage the values to plot (e.g. intersection cardinalities.)
    This helps decoupling and coherence, separating the plotting functions from those actually producing the values to plot.

--- pyUpset.py
+++ pyUpset.py
@@ -6,7 +6,8 @@ from matplotlib import gridspec
 from itertools import chain, combinations
 import pandas as pd
 
-def plot(sets, set_names, sort_by='size', inters_size_bounds=(0, np.inf), inters_degree_bounds=(1, np.inf)):
+
+def plot(data_dict, columns, sort_by='size', inters_size_bounds=(0, np.inf), inters_degree_bounds=(1, np.inf)):
     """
     Wrapper function that initialises an UpSet class and calls its plot() method with the passed parameters.
 
@@ -19,20 +20,18 @@ def plot(sets, set_names, sort_by='size', inters_size_bounds=(0, np.inf), inters
     be plotted.
     :return: figure and axes containing the plots.
     """
-    return UpSet(sets, set_names).plot(sort_by, inters_size_bounds=inters_size_bounds,
-                                inters_degree_bounds=inters_degree_bounds)
+    return UpSet(__PlotData(data_dict)).plot(columns, sort_by, inters_size_bounds=inters_size_bounds,
+                                       inters_degree_bounds=inters_degree_bounds)
 
 
 class UpSet():
-
-    def __init__(self, data_dict):
+    def __init__(self, plot_data):
         """
         Class linked to a data set; it contains the methods to produce plots according to the UpSet representation.
-
-        :param data_dict: dict. {'name': pandas DataFrame}
+        :type plot_data: __PlotData
+        :param plot_data: PlotData object
         """
-        self.data_dict = data_dict
-        self.inters_degree_bounds, self.inters_size_bounds = None, None
+        self.plot_data = plot_data
 
     def _base_sets_plot(self, ax, sorted_sets, sorted_set_names):
         ax.invert_xaxis()
@@ -165,57 +164,6 @@ class UpSet():
             for s in ax.spines.values():
                 s.set_visible(False)
 
-    def _compute_intersections(self, set_dict, inters_size_bounds=(0, np.inf), inters_degree_bounds=(0, np.inf)):
-        """
-        Computes the intersections of the sets passed to the class discarding those beyond the size or degree bounds.
-
-        :param set_dict: dict. {'name':'set'}
-        :param inters_size_bounds: tuple. The minimum and maximum (inclusive) size allowed for an intersection to be
-        plotted.
-        :param inters_degree_bounds: tuple. The minimum and maximum (inclusive) degree allowed for an intersection to
-        be plotted.
-        :returns self.
-        """
-        in_sets_list = []
-        out_sets_list = []
-        inters_sizes = []
-        inters_degrees = []
-
-        set_names = set_dict.keys()
-
-        for col_num, in_sets in enumerate(chain.from_iterable(
-                combinations(set_names, i) for i in np.arange(1, len(set_dict) + 1))):
-
-            inters_degrees.append(len(in_sets))
-
-            in_sets_list.append(in_sets)
-            in_sets = list(in_sets)
-
-            out_sets = set(set_names).difference(in_sets)
-            out_sets_list.append(tuple(out_sets))
-
-            exclusive_intersection = set(set_dict[in_sets.pop()])
-            for s in in_sets:
-                exclusive_intersection.intersection_update(set_dict[s])
-            for s in out_sets:
-                exclusive_intersection.difference_update(set_dict[s])
-            inters_sizes.append(len(exclusive_intersection))
-
-        inters_sizes = np.array(inters_sizes)
-        inters_degrees = np.array(inters_degrees)
-
-        size_clip = (inters_sizes <= inters_size_bounds[1]) & (inters_sizes >= inters_size_bounds[0]) & (
-            inters_degrees >= inters_degree_bounds[0]) & (inters_degrees <= inters_degree_bounds[1])
-
-        self.in_sets_list = np.array(in_sets_list)[size_clip]
-        self.out_sets_list = np.array(out_sets_list)[size_clip]
-        self.inters_sizes = inters_sizes[size_clip]
-        self.inters_degrees = inters_degrees[size_clip]
-#        self.inters_size_bounds, self.inters_degree_bounds = inters_size_bounds, inters_degree_bounds
-
-        return self
-
-
     def _create_coordinates(self, sets, inters_sizes):
         self.rows = len(sets)
         self.cols = len(inters_sizes)
@@ -249,36 +197,15 @@ class UpSet():
         :return: figure and list of axes produced.
         """
 
-        sets = []
-        set_names = []
-        for (dfname, setname) in columns.items():
-            sets.append(set(self.data_dict[dfname][setname].values))
-            set_names.append('%s[%s]' % (dfname, setname))
-        sets = np.array(sets)
-        set_names = np.array(set_names)
+        ordered_base_sets, ordered_base_set_names = self.plot_data.extract_base_sets_data(columns)
+        ordered_inters_sizes, ordered_in_sets, ordered_out_sets = self.plot_data \
+            .extract_intersections_data(sort_by, inters_size_bounds, inters_degree_bounds)
 
-        # if (self.inters_size_bounds != inters_size_bounds) and (self.inters_degree_bounds != inters_degree_bounds):
-        self._compute_intersections(dict(zip(set_names, sets)), inters_size_bounds, inters_degree_bounds)
-
-        if sort_by == 'size':
-            order = np.argsort(self.inters_sizes)[::-1]
-        elif sort_by == 'degree':
-            order = np.argsort(self.inters_degrees)
-
-        ordered_inters_sizes = self.inters_sizes[order]
-        ordered_in_sets = self.in_sets_list[order]
-        ordered_out_sets = self.out_sets_list[order]
-
-        self._create_coordinates(sets, ordered_inters_sizes)
-
+        self._create_coordinates(ordered_base_sets, ordered_inters_sizes)
         ax_intbars, ax_intmatrix, ax_setsize, fig = self._prepare_figure()
 
-        base_sets_order = np.argsort([len(x) for x in sets])[::-1]
-        sorted_sets = sets[base_sets_order]
-        sorted_sets_names = set_names[base_sets_order]
-        set_row_map = dict(zip(sorted_sets_names, self.y_values))
-
-        ylim = self._base_sets_plot(ax_setsize, sorted_sets, sorted_sets_names)
+        set_row_map = dict(zip(ordered_base_set_names, self.y_values))
+        ylim = self._base_sets_plot(ax_setsize, ordered_base_sets, ordered_base_set_names)
         xlim = self._inters_sizes_plot(ax_intbars, ordered_inters_sizes)
         self._inters_matrix(ax_intmatrix, ordered_in_sets, ordered_out_sets, xlim, ylim, set_row_map)
 
@@ -286,3 +213,90 @@ class UpSet():
 
     def _scatter(self, ax, xvar, yvar):
         pass
+
+
+class __PlotData:
+    def __init__(self, data_dict):
+        self.data_dict = data_dict
+        self.ordered_base_set_names, self.ordered_base_sets, self.set_dict = np.repeat(None, 3)
+
+    def extract_base_sets_data(self, columns):
+        # Extract the columns as sets to intersect
+        sets = []
+        set_names = []
+        if len(columns) != len(self.data_dict):
+            raise ValueError("columns argument must specify one column per DataFrame")
+        for (dfname, setname) in columns.items():
+            sets.append(set(self.data_dict[dfname][setname].values))
+            set_names.append('%s[%s]' % (dfname, setname))
+
+        base_sets = np.array(sets)
+        base_set_names = np.array(set_names)
+
+        # order base sets and store
+        base_sets_order = np.argsort([len(x) for x in sets])[::-1]
+        self.ordered_base_sets = base_sets[base_sets_order]
+        self.ordered_base_set_names = base_set_names[base_sets_order]
+        self.set_dict = dict(zip(self.ordered_base_set_names, self.ordered_base_sets))
+
+        return self.ordered_base_sets, self.ordered_base_set_names
+
+    def extract_intersections_data(self, sort_by, inters_size_bounds, inters_degree_bounds):
+        """
+
+        :param columns: dict of columns, one k-v pair per dataframe.
+        :return:
+        """
+
+        try:
+            if self.ordered_base_sets or self.ordered_base_set_names: pass
+        except AttributeError as e:
+            print("Base sets must be extracted before it's possible to compute their intersections.")
+            raise
+
+        # Compute the intersection values
+        in_sets_list = []
+        out_sets_list = []
+        inters_sizes = []
+        inters_degrees = []
+
+        for col_num, in_sets in enumerate(chain.from_iterable(
+                combinations(self.ordered_base_set_names, i) for i in np.arange(1, len(self.ordered_base_sets) + 1))):
+
+            inters_degrees.append(len(in_sets))
+            in_sets_list.append(in_sets)
+            in_sets = list(in_sets)
+            out_sets = set(self.ordered_base_set_names).difference(in_sets)
+            out_sets_list.append(tuple(out_sets))
+            exclusive_intersection = set(self.set_dict[in_sets.pop()])
+
+            for s in in_sets:
+                exclusive_intersection.intersection_update(self.set_dict[s])
+            for s in out_sets:
+                exclusive_intersection.difference_update(self.set_dict[s])
+
+            inters_sizes.append(len(exclusive_intersection))
+
+        inters_sizes = np.array(inters_sizes)
+        inters_degrees = np.array(inters_degrees)
+
+        size_clip = (inters_sizes <= inters_size_bounds[1]) & (inters_sizes >= inters_size_bounds[0]) & (
+            inters_degrees >= inters_degree_bounds[0]) & (inters_degrees <= inters_degree_bounds[1])
+
+        in_sets_list = np.array(in_sets_list)[size_clip]
+        out_sets_list = np.array(out_sets_list)[size_clip]
+        inters_sizes = inters_sizes[size_clip]
+        inters_degrees = inters_degrees[size_clip]
+
+        # sort as requested
+        if sort_by == 'size':
+            order = np.argsort(inters_sizes)[::-1]
+        elif sort_by == 'degree':
+            order = np.argsort(inters_degrees)
+
+        # store ordered data
+        self.ordered_inters_sizes = inters_sizes[order]
+        self.ordered_in_sets = in_sets_list[order]
+        self.ordered_out_sets = out_sets_list[order]
+
+        return self.ordered_inters_sizes, self.ordered_in_sets, self.ordered_out_sets
